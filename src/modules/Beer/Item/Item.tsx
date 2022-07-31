4dbc8744d49251b6ebeaa82a8cc09e45e4f90fc8
Inconsumable <inconsumable1812@gmail.com>
2022-07-31 10:49:23 +0800

    improved title

--- src/modules/Beer/Item/Item.tsx
+++ src/modules/Beer/Item/Item.tsx
@@ -2,13 +2,18 @@ import type { NextPage } from 'next';
 
 import { PageHead } from 'src/shared/components';
 import { Beer as BeerComponent } from 'src/features/beer';
+import { useAppSelector } from 'src/app/hooks';
+import { selectBeer } from 'src/features/beer/redux/slice';
 
 import styles from './Item.module.scss';
 
 const Item: NextPage = () => {
+  const { currentBeer } = useAppSelector(selectBeer);
+
+  const title = currentBeer !== null ? currentBeer.name : 'Beer Item';
   return (
     <>
-      <PageHead title={'Beer Item'} />
+      <PageHead title={title} />
       <div className={`${styles.root}`}>
         <BeerComponent view="item"></BeerComponent>
       </div>
